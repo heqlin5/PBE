fcd51c173fe81bd505708b78539a2f83e9ec8acf
heqlin5 <114636518+heqlin5@users.noreply.github.com>
2026-02-06 10:17:27 +0800

    Update sba_test_regdb.sh

--- sba_test_regdb.sh
+++ sba_test_regdb.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python regdb_test_PBE.py -b 64 -a agw -d  regdb_rgb --iters 200 --momentum 0.1 --eps 0.6 --num-instances 16 --checkpoint-dir /home/haorui/hql/PBE_end/logs/_regdb_ends
+CUDA_VISIBLE_DEVICES=1 python regdb_test_PBE.py -b 64 -a agw -d  regdb_rgb --iters 200 --momentum 0.1 --eps 0.6 --num-instances 16 --checkpoint-dir ./logs/regdb
