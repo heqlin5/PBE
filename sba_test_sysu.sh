c05839f8dcb1259b9f3dbc110bc467a9c113d9a6
heqlin5 <114636518+heqlin5@users.noreply.github.com>
2026-02-06 10:16:51 +0800

    Update sba_test_sysu.sh

--- sba_test_sysu.sh
+++ sba_test_sysu.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=2 python sysu_test_PBE.py -b 32 -a agw -d  sysu_all --iters 200 --momentum 0.1 --eps 0.6 --num-instances 16 --checkpoint-dir /home/haorui/hql/PBE_end/logs/sysu_ends
+CUDA_VISIBLE_DEVICES=2 python sysu_test_PBE.py -b 32 -a agw -d  sysu_all --iters 200 --momentum 0.1 --eps 0.6 --num-instances 16 --checkpoint-dir ./logs/sysu
